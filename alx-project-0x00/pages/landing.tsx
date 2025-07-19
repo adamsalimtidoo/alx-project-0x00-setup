d967fa0b1b9560c000847043fd74965ea2e55761
adamsalimtidoo <119486996+adamsalimtidoo@users.noreply.github.com>
2025-07-19 00:25:36 +0000

    updated

---
+++ alx-project-0x00/pages/landing.tsx
@@ -0,0 +1,9 @@
+//basic function
+const Landing: React.FC = () => {
+  return (
+    <div>
+      <h1 className=" text-xl font-extralight">Landing Page</h1>
+    </div>
+  );
+};
+export default Landing;
