a2cb86fd61ca210c1e00c9063bd7794824340450
adamsalimtidoo <119486996+adamsalimtidoo@users.noreply.github.com>
2025-07-19 20:16:28 +0000

    updated

--- alx-project-0x00/components/Button.tsx
+++ alx-project-0x00/components/Button.tsx
@@ -1,14 +1,12 @@
 import React from "react";
 
-type ButtonProps = {
-  title: string;
-  styles: string;
+type ButtonProps = {title: string; styles: string;
 };
 
 const Button = ({ title, styles }: ButtonProps) => {
   return (
     <button
-      className={`px-4 py-2 font-semibold text-white bg-blue-600 hover:bg-blue-700 transition rounded-lg${style}`}
+      className={`px-4 py-2 font-semibold text-white bg-blue-600 hover:bg-blue-700 transition rounded-lg${styles}`}
     >
       {title}
     </button>
--- alx-project-0x00/interfaces/index.ts
+++ alx-project-0x00/interfaces/index.ts
@@ -3,4 +3,5 @@ import React from "react";
 
 export interface PillProps {
   title: string;
+  styles: string;
 }
