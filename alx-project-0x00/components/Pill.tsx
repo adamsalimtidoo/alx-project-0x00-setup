83153e1f25dfc07b490b3d4678b7d0301dddb417
adamsalimtidoo <119486996+adamsalimtidoo@users.noreply.github.com>
2025-07-19 00:47:57 +0000

    updated

--- alx-project-0x00/components/Pill.tsx
+++ alx-project-0x00/components/Pill.tsx
@@ -1,1 +1,10 @@
-//hallo world
+//Update file
+const Pill: React.FC = () => {
+  return (
+    <div className=" flex justify-center  items-center bg-[#F9F9F9] px-3 w-auto h-[27px] rounded-full">
+      <p className=" text-sm ">Title</p>
+    </div>
+  );
+};
+
+export default Pill;
